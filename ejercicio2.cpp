38dbf3691f4797e088f74800c9d3dddb6cfd93e8
Julio Flamenco <jflamenco@outlook.com>
2020-05-29 21:10:56 -0600

    Agregando ejercicio 2

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -8,18 +8,30 @@ int main() {
     int n = 2;
     int mBidemensional[2][2];
 
-    mBidemensional[0][0] = 4;
-    mBidemensional[1][0] = 6;    
-    mBidemensional[0][1] = 8;
-    mBidemensional[1][1] = 2;
+    cout << "Ingrese un numero del 1 al 10" << endl;
+    cin>>n;
 
-
-    cout << " Impresion en forma de tabla de N: " << n <<  endl;
     for (int fila = 0; fila < 2; fila++ ) {
         for (int col = 0; col < 2; col++ ) {
-            cout << mBidemensional[fila][col] << "   ";
+           
+          int multiplo;
+          do{
+            multiplo = random() % 10;
+          } while ((multiplo % n) == 0);          
+          mBidemensional[fila][col] = multiplo;
         }
-        cout << (mBidemensional[fila][0] * mBidemensional[fila][1]) << endl; 
     }
+    cout << " Impresion en forma de tabla" << endl;
+
+    for (int fila = 0; fila < 2; fila++ ) {
+        for (int col = 0; col < 2; col++ ) {
+            
+            cout << mBidemensional[fila][col] << "   ";
+
+        }
+
+        cout << (mBidemensional[fila][0] * mBidemensional[fila][1]) << " | " << endl;
+    }
+    
 
 }
